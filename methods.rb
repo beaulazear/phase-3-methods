9d53badd5fc4b8866672956fee12e3d3545e4614
Beau <beaulazear@gmail.com>
2023-02-27 16:37:17 -0500

    commit tree 8d227f2d8ddcf018d01a0f8e17ed6e1d2d955b19

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+#1
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+#2
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+#3
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+#4
+
+def add x, y
+    x + y
+end
+
+#5
+
+def halve n
+    if n.class != Integer
+        return nil
+    end
+
+    n / 2
+end
